c4d777b3967081cb4f22020cc4bd578b5658348c
Nate <31741696+n-ate@users.noreply.github.com>
2020-05-02 10:11:18 -0700

    Create CustomInputTagHelper.cs
    Extends the existing asp-for InputTagHelper to support the description value of the DisplayAttribute.

---
+++ CustomInputTagHelper.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace CustomTagHelpers
+{
+    /// <summary>
+    /// Extends the default asp-for attribute by adding description support.
+    /// </summary>
+    /// <example>
+    /// Example _ViewImports.cshtml:
+    ///     @namespace DefaultNamespace.Pages
+    ///     @addTagHelper*, Microsoft.AspNetCore.Mvc.TagHelpers
+    ///     @removeTagHelper Microsoft.AspNetCore.Mvc.TagHelpers.InputTagHelper, Microsoft.AspNetCore.Mvc.TagHelpers
+    ///     @addTagHelper CustomTagHelpers.CustomInputTagHelper, CustomTagHelpers
+    ///     
+    /// Example Markup Usage:
+    ///     &lt;input asp-for="Property" class="form-control" readonly />
+    /// </example>
+    [HtmlTargetElement("input", Attributes = FOR_ATTRIBUTE_NAME, TagStructure = TagStructure.WithoutEndTag)]
+    public class CustomInputTagHelper : InputTagHelper
+    {
+        private const string FOR_ATTRIBUTE_NAME = "asp-for";
+
+        public CustomInputTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var metadata = base.For.Metadata;
+
+            if (metadata == null)
+            {
+                throw new InvalidOperationException(string.Format("No provided metadata " + FOR_ATTRIBUTE_NAME));
+            }
+
+            if (!string.IsNullOrWhiteSpace(metadata.Description))
+            {
+                output.Attributes.SetAttribute("title", metadata.Description);
+            }
+            base.Process(context, output);
+        }
+    }
+}
